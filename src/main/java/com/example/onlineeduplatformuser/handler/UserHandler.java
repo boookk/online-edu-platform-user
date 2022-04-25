7125feea9e073a24c97f0e837cce7b1bd7cdbb37
Jeayoeb Kim <jykim@atones.co.kr>
2022-04-25 13:36:55 +0900

    refactor: 핸들러 인터페이스화

--- src/main/java/com/example/onlineeduplatformuser/handler/UserHandler.java
+++ src/main/java/com/example/onlineeduplatformuser/handler/UserHandler.java
@@ -1,32 +1,16 @@
 package com.example.onlineeduplatformuser.handler;
 
-import com.example.onlineeduplatformuser.dto.UserLoginResponse;
-import com.example.onlineeduplatformuser.dto.UserRegistrationResponse;
-import com.example.onlineeduplatformuser.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
-public class UserHandler {
+public interface UserHandler {
 
-    private final UserService userService;
+    Mono<ServerResponse> register(ServerRequest serverRequest);
 
-    public Mono<ServerResponse> register(ServerRequest serverRequest) {
-        return ServerResponse.ok().body(userService.registrationTeacher(serverRequest),
-                UserRegistrationResponse.class);
-    }
+    Mono<ServerResponse> join(ServerRequest serverRequest);
 
-    public Mono<ServerResponse> join(ServerRequest serverRequest) {
-        return ServerResponse.ok().body(userService.registrationStudent(serverRequest),
-                UserRegistrationResponse.class);
-    }
-
-    public Mono<ServerResponse> login(ServerRequest serverRequest) {
-        return ServerResponse.ok().body(userService.loginService(serverRequest),
-                UserLoginResponse.class);
-    }
+    Mono<ServerResponse> login(ServerRequest serverRequest);
 }
