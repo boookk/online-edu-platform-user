165be60b770b6afd4bbb2ede60916ca0cb2fd97f
박정수 <wjdtn4383@daum.net>
2022-04-26 21:56:51 +0900

    유레카 클라이언트 추가

--- src/main/java/com/example/onlineeduplatformuser/OnlineEduPlatformUserApplication.java
+++ src/main/java/com/example/onlineeduplatformuser/OnlineEduPlatformUserApplication.java
@@ -2,8 +2,10 @@ package com.example.onlineeduplatformuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OnlineEduPlatformUserApplication {
 
     public static void main(String[] args) {
